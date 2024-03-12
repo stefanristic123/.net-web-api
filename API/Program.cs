75c2f369a8bea52b290f35179b8e38ace836c0b1
Stefan Ristic <titanium955@gmail.com>
2024-03-12 17:16:54 +0100

    15 - Adding Identity, login and register

--- API/Program.cs
+++ API/Program.cs
@@ -2,9 +2,12 @@ using API.Data;
 using API.Extensions;
 using API.Helpers;
 using API.Middleware;
+using Core.Entities.Identity;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Data.Repository;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,34 +16,39 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
-app.UseCors("CorsPolicy");
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
-
 app.UseSwagger();
 app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-   
-app.UseAuthorization();
 app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
 var logger = services.GetRequiredService<ILogger<Program>>();
-try{
+try
+{
     await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
-}catch (Exception ex){
-   logger.LogError(ex, "An error occured during migration");
+    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
 }
-
+catch (Exception ex)
+{
+    logger.LogError(ex, "An error occured during migration");
+}
 app.Run();
